3c92d257fdaf21712d77add46c6af002dce97a23
shahbain21 <shahbain@umich.edu>
2025-01-04 20:36:07 -0500

    Initial Commit

---
+++ fitQuest/src/main/java/com/workout/fitQuest/FitQuestApplication.java
@@ -0,0 +1,13 @@
+package com.workout.fitQuest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitQuestApplication.class, args);
+	}
+
+}
---
+++ fitQuest/src/test/java/com/workout/fitQuest/FitQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workout.fitQuest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
