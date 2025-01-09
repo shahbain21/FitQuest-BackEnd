bd10b3720d4a446d0471a8035b385ae0e283c9ab
shahbain21 <shahbain@umich.edu>
2025-01-08 22:44:06 -0500

    Created database

---
+++ fitQuest/src/main/java/com/workout/fitQuest/Entities/User.java
@@ -0,0 +1,20 @@
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String passwordHash;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
